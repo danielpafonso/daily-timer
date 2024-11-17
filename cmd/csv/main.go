63f35b5d95ec0b593c3b229e5e8d0f2564b63754
Daniel Afonso <62136600+danielpafonso@users.noreply.github.com>
2024-11-17 21:33:04 +0000

    preparing version bump

--- cmd/csv/main.go
+++ cmd/csv/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	Version string = "0.0.3"
+	Version string = "0.1.0"
 )
 
 func main() {
--- cmd/sqlite/main.go
+++ cmd/sqlite/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	Version string = "0.0.3"
+	Version string = "0.1.0"
 )
 
 func main() {
